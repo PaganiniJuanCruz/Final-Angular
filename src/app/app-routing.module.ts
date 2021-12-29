80a8adfda704b7fb020a9a7f81ba8cc67b5de062
juancruzpaganini <juancruzpaganini4@gmail.com>
2021-12-29 16:16:06 -0300

    registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path:'register',
     component: RegisterComponent
+  },
+  { 
+    path:'cart',
+    component: CartComponent
   },
   {
     path: '',
